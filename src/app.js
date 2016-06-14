0c0ff80ed46db6ff1d0023fe531efe73031fefb3
Selim Oruc <selim.oruc40@gmail.com>
2016-06-14 16:59:43 +0200

    added (now i can use html)

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 var Sequelize = require('sequelize');
 var express = require('express');
 var bodyParser = require('body-parser');
+var app = express()
+app.use(bodyParser.urlencoded ({extended: false}));
+app.use(express.static('./static'));
 
-var sequelize = new Sequelize('shisha', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+//app.use( express.static( __dirname + '/static' ) )
+//app.use( bodyParser.urlencoded( { extended: false } ) )
+
+
+// this means thats about the database called shisha
+
+var sequelize = new Sequelize('shisha', 'postgres', "Selim0ruc", {
 	host: 'localhost',
 	dialect: 'postgres',
 	define: {
@@ -10,6 +19,7 @@ var sequelize = new Sequelize('shisha', process.env.POSTGRES_USER, process.env.P
 	}
 });
 
+//this makes a table called lounges (he adds an "s" automatically)
 var Lounge = sequelize.define('lounge', {
 	loungeName: Sequelize.STRING,
 	tel: Sequelize.INTEGER,
@@ -18,39 +28,29 @@ var Lounge = sequelize.define('lounge', {
 	houseNumber: Sequelize.INTEGER,
 	city: Sequelize.STRING
 });
-
+// this makes a table called menus (he adds an "s" automatically
 var Menu = sequelize.define('menu', {
 	selectionFood: Sequelize.TEXT,
 	selectionDrinks: Sequelize.TEXT,
 	selectionShisha: Sequelize.TEXT
 })
 
+//this is te relationship between tables
 Lounge.hasMany(Menu);
 Menu.belongsTo(Lounge);
 
-var app = express();
 
-app.set( 'views', __dirname + '/views' )
+
+app.set ("views", "src/views");
 app.engine( 'html', require( 'ejs' ).renderFile )
 
-app.use( express.static( __dirname + '/static' ) )
-app.use( bodyParser.urlencoded( { extended: false } ) )
 
 app.get ( '/', function ( request, response ) {
 	response.render ( 'index.html' )  
 } )
 
-sequelize.sync({force: false}).then(function () {
-	Lounge.create({
-		username: "roger",
-		email: "roger@nycda.cool",
-		password: "password"
-	}).then(function () {
-		var server = app.listen(3000, function () {
-			console.log('Example app listening on port: ' + server.address().port);
-		});
-	});
-}, function (error) {
-	console.log('sync failed: ');
-	console.log(error);
-});
+sequelize.sync({force: false})
+var server = app.listen(3000 , function (){
+    
+        console.log("example app listening on port : " + server.address().port)
+})
